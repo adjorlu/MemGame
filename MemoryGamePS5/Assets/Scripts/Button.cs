c54841226677f08b315eb5983c22ab00ea66a0c4
Francesco Ganis <35145285+frandrums@users.noreply.github.com>
2023-02-10 15:39:43 +0100

    Added button interaction with DualSense in main menu

---
+++ MemoryGamePS5/Assets/Scripts/Button.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Button : MonoBehaviour
+{
+    [SerializeField] GameObject mouse;
+
+    public bool hovered = false;
+    public bool pressed = false;
+    
+    void OnCollisionStay2D(Collision2D targetObj)
+    {
+        if (targetObj.gameObject.tag == "selector")
+        {
+            hovered= true;
+        }
+
+        if (targetObj.gameObject.tag == "selector" && mouse.GetComponent<mouseController>().xPressed == true)
+        {
+            pressed= true;
+        }
+
+        if (targetObj.gameObject.tag == "selector" && mouse.GetComponent<mouseController>().xPressed == false)
+        {
+            pressed = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D targetObj)
+    {
+        hovered = false;
+        pressed = false;
+
+    }
+}
